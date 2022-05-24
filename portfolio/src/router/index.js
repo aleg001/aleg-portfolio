ea7e8e2efe0fa60959bae85b55b378bfea389b05
Ale Gómez <gom20347@uvg.edu.gt>
2022-05-23 23:00:03 -0600

    added router

---
+++ portfolio/src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'homepage',
+    component: () => import('@/App.vue'),
+  },
+  {
+    path: '/about',
+    name: 'AboutView',
+    component: () => import('@/views/AboutView.vue'),
+  },
+
+  {
+    path: '/portfolio',
+    name: 'PortfolioView',
+    component: () => import('@/views/PortfolioView.vue'),
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+})
+
+export default router
---
+++ portfolio/src/views/AboutView.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <v-app-bar app color="white" style="position: fixed">
+      <Navigator />
+      <v-spacer></v-spacer>
+      <bar-buttons></bar-buttons>
+    </v-app-bar>
+
+    <v-main style="background-color: #96a39b"> </v-main>
+  </v-app>
+</template>
+
+<script>
+import BarButtons from '../components/BarButtons.vue'
+import Navigator from '../components/Navigator.vue'
+
+export default {
+  name: 'AboutView',
+
+  components: {
+    BarButtons,
+    Navigator,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Abel&family=Cantarell&family=Oswald:wght@200&display=swap');
+</style>
+
+<!--
+  FONTS:
+  1) Titulos
+  2) Subtitulos
+  3) Parrafos
+font-family: 'Oswald', sans-serif;
+font-family: 'Abel', sans-serif;
+font-family: 'Cantarell', sans-serif;
+
+-->
